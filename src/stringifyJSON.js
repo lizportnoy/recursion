19c52321d2fdd5068babbf12f001cf5800e6f9a2
Liz Portnoy <lizport10@gmail.com>
2014-10-02 14:01:12 -0400

    completed Stringify JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,33 +2,53 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
+
 var stringifyJSON = function(obj) {
-/*
-
-
-	var objarray = [];
-	for (var k in obj) {
-		objarray.push([k,obj[k]]);
+// checks if number or boolean, and if so returns string version
+	if (typeof obj === "number" || typeof obj === "boolean" ) {
+		return obj.toString();
+	}
+// checks if object is null, and returns string "null"
+	else if (obj === null) {
+		return "null";
+	}
+// checks if object is string, and adds extra set of quotation marks
+	else if (typeof obj === "string") {
+		return '\"' + obj + '\"';
+	}
+//checks if object is function or undefined = if so returns null
+	else if (typeof obj === "function" || obj === undefined) {
+		return null;
+	}
+// checks if object is array
+	else if (Array.isArray(obj)) {
+		var arrayWrap = '['; // begins string version of array
+		if (obj.length > 0) { // checks that array is not empty
+			for (var i = 0; i < obj.length; i ++) {
+				arrayWrap += stringifyJSON(obj[i]) + ','; // stringifies each array element so they can be combined
+				} 
+			arrayWrap = arrayWrap.slice(0,arrayWrap.length - 1); // cuts off last comma in array
+		}
+		return arrayWrap + ']' // closes array
+	}
+// checks if object is object
+	else if (obj.toString().slice(1,7)=== 'object') {
+		var objWrap = '{'; // begins object version of array
+		if (Object.keys(obj).length > 0) { // checks that object is not empty
+			for (var k in obj) {
+				if (typeof obj[k] === "function" || obj[k] === undefined) {
+					delete obj[k]; // checks for keys containing values that are undefined or functions and removes the key-value pair
+				}
+				else {
+				objWrap += stringifyJSON(k) + ":" + stringifyJSON(obj[k]) + ','; // stringifies each key-value pair so they can be combined
+				}
+			} 
+			if (objWrap.length > 1) { // checks that object is not empty following deletion of functions/undefined
+			objWrap = objWrap.slice(0,objWrap.length - 1); // cuts off final comma
+			} 
+		}
+		return objWrap + '}';
 	}
 
-	var stringifiedObj = {}; // 
-
-	var stringify = function (arr) {
-
-		if (objarray.length < 1) {
-			// base case returns stringified object
-			return stringifiedObj.toString();
-
-		}
-
-		else  {
-			if (objarray[0][0]
-
-			objarray.splice(0,1);
-			return stringify(objarray);
-		}
-
-  	return stringifyJSON(obj) }
-
-  	*/
 };
